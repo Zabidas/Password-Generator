ca2e37cae43ba30b9e6033ef8f0b4887fbd02d53
Zabidas <93616180+Zabidas@users.noreply.github.com>
2023-04-01 22:02:48 +0300

    Add files via upload

---
+++ PasswordGen.py
@@ -0,0 +1,27 @@
+import random
+
+lst = ('q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c',
+       'v', 'b', 'n', 'm', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*',
+       '(', ')', '~', '_', '-', '+', '=', '.', '[', ']', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S',
+       'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M')
+
+
+def generate_password():
+    random_item = (random.choice(lst), random.choice(lst), random.choice(lst), random.choice(lst), random.choice(lst),
+                   random.choice(lst), random.choice(lst), random.choice(lst), random.choice(lst), random.choice(lst),
+                   random.choice(lst), random.choice(lst))
+    final = ''.join(random_item)
+    return final
+
+
+a = input("Want a password? y/n: ")
+if a == 'y':
+    x = 2
+    while x > 1:
+        print(generate_password())
+        x2 = input("One more? 2=yes 1=no: ")
+        x = int(x2)
+    else:
+        print('bye')
+else:
+    print('bye')
